0dc34ef661d8405684578265a11a4f43a2a921e1
Elochukwu2467 <anawanaloch@gmail.com>
2023-08-01 07:57:56 -0700

    chessboard

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_chessboard - Entry point
+ * @a: array
+ * Return: Always o (success)
+ */
+void print_chessboard(char (*a)[8])
+{
+	int i, n;
+
+	for (i = 0; i < 8; i++)
+	{
+		for (n = 0; n < 8; n++)
+			putchar(a[i][n]);
+		putchar('\n');
+	}
+}
